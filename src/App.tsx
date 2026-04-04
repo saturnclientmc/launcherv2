f822674bff9c357e2d75d1d363c321235ba49d98
Klesti Selimaj <klesti.selimay@icloud.com>
2026-04-04 19:19:29 +0200

    Fixed section scroll

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
           accountRef={accountRef}
         />
 
-        <div className="flex-1 p-8">
+        <div className="flex-1 p-8 overflow-y-scroll">
           {active === "play" && selectedVersion && (
             <PlaySection version={selectedVersion} />
           )}
